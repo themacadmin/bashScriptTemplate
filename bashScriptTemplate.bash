0e93a42e3fa4428aeaa2af64d64f4096d036e63b
The Mac Admin <tma@themacadmin.com>
2015-10-23 11:19:35 -0700

    Initial template
    Structure!

---
+++ bashScriptTemplate.bash
@@ -0,0 +1,87 @@
+#!/bin/bash
+#
+##########################################################################################
+#
+# Header begins
+#
+##########################################################################################
+#
+# Copyright (c) 2015, Miles A. Leacy IV.  All rights reserved.
+#
+#     This script may be copied, modified, and distributed freely as long as this header
+#     remains intact and modifications are publicly shared with the Mac administrators'
+#     community at large.
+#
+#     This script is provided "as is".  The author offers no warranty or guarantee of any
+#     kind.
+#
+#     Use of this script is at your own risk.  The author takes no responsibility for loss
+#     of use, loss of data, loss of job, loss of socks, the onset of armageddon, or any
+#     other negative effects.
+#
+#     Test thoroughly in a lab environment before use on production systems.
+#     When you think it's ok, test again.  When you're certain it's ok, test twice more.
+#
+##########################################################################################
+#
+# About This Script
+#
+# Name
+#	scriptName.bash -- Short description of script's purpose.
+#
+# Usage
+#	sudo scriptName.bash
+#
+# DESCRIPTION
+#	Long description of script's purpose and function.
+#	
+#
+##########################################################################################
+#
+# History
+#
+#	Version: x.y
+#
+#	- Created by name on date
+#   - Modified by name on date
+#		- changes in this modification
+#
+##########################################################################################
+#
+# Header ends
+#
+##########################################################################################
+#
+# Declare Variables
+#
+##########################################################################################
+
+x="y"
+
+##########################################################################################
+#
+# Script begins
+#
+##########################################################################################
+
+echo "hello"
+
+##########################################################################################
+#
+# Particular subroutine begins
+#
+##########################################################################################
+
+if [ "$x" == "y" ]; then
+	echo "x = "$x
+else 
+	echo "What's going on?"
+fi
+
+##########################################################################################
+#
+# Particular subroutine ends
+#
+##########################################################################################
+
+exit 0
